5a97b4139536d5cb4cef6b025f7c0e7e22ff625c
Frank Fu <frankfu1122@gmail.com>
2019-05-22 16:17:28 +1000

    Add project files.

---
+++ MyEchoService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEchoService
+{
+    [ServiceContract]
+    public interface IMyEchoService
+    {
+        string EchoPlease(string s);
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
