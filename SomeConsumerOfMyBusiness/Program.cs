8dbbb2313fa738a4ec01c0b1a4a57439edbaae5e
Frank Fu <frankfu1122@gmail.com>
2019-05-22 19:28:42 +1000

    Add ability to choose Transport mechanism

--- SomeConsumerOfMyBusiness/Program.cs
+++ SomeConsumerOfMyBusiness/Program.cs
@@ -13,18 +13,42 @@ namespace SomeConsumerOfMyBusiness
     {
         static void Main(string[] args)
         {
-            //var channelFactory = GetHttpChannelFactory<IMyEchoService>("http://localhost:8880/MyEchoService");
-            //var channelFactory = GetNetTCPChannelFactory<IMyEchoService>("net.tcp://localhost:8881/MyEchoService");
+            ChannelFactory<IMyEchoService> channelFactory = null;
 
-            var channelFactory = GetMSMQChannelFactory<IMyEchoService>("net.msmq://localhost/private/MyEchoService");
+            string[] validTransportSelections = new string[] { "1", "2", "3" };
+
+            Console.WriteLine($"Please choose mode Transport");
+            Console.WriteLine($"1) HTTP ");
+            Console.WriteLine($"2) TCP ");
+            Console.WriteLine($"3) MSMQ ");
+
+            string selectedTransport = null;
+            bool validTransportOptionSelected = false;
+
+            while (!validTransportOptionSelected)
+            {
+                selectedTransport = Console.ReadLine();
+                validTransportOptionSelected = !(selectedTransport == null || selectedTransport == "" || !validTransportSelections.Contains(selectedTransport));
+            }
+
+            if (selectedTransport == "1")
+                channelFactory = GetHttpChannelFactory<IMyEchoService>("http://localhost:8880/MyEchoService");
+            if (selectedTransport == "2")
+                channelFactory = GetNetTCPChannelFactory<IMyEchoService>("net.tcp://localhost:8881/MyEchoService");
+            if (selectedTransport == "3")
+                 channelFactory = GetMSMQChannelFactory<IMyEchoService>("net.msmq://localhost/private/MyEchoService");
+
             var channel = channelFactory.CreateChannel();
 
-            Console.WriteLine($"Type a message and it will be sent to {channelFactory.Endpoint.Address}");
+
+           Console.WriteLine($"Type a message and it will be sent to {channelFactory.Endpoint.Address}");
             while(true)
             {
                 var input = Console.ReadLine();
-                if(input != "q")
-                    channel.EchoPlease(input);
+                if (input == "q")
+                    break;
+
+                channel.EchoPlease(input);
             }
         }
 
@@ -46,7 +70,7 @@ namespace SomeConsumerOfMyBusiness
 
         private static ChannelFactory<T> GetMSMQChannelFactory<T>(string endPointAddress)
         {
-            var binding = new NetMsmqBinding(securityMode: NetMsmqSecurityMode.None) { ExactlyOnce = false };
+            var binding = new NetMsmqBinding(securityMode: NetMsmqSecurityMode.None) { ExactlyOnce = false }; // Needs to match exactly with the Service Host!
             var channelFactory = new ChannelFactory<T>(binding, endPointAddress);
 
             return channelFactory;
